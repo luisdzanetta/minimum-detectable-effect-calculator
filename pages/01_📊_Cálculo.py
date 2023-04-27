2ba6af5f47b8f63b53ab477aa001cb1daa024be7
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-26 22:27:01 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -1,14 +1,15 @@
 import streamlit as st
-from scipy.stats import norm
 
 st.title("Cálculo do MDE")
 
 st.write("O **MDE**, é o tamanho mínimo da diferença entre as taxas de conversão das variantes e do grupo de controle que pode ser detectado com um nível de significância alpha e um poder de teste beta. Em outras palavras, se a diferença entre as taxas de conversão das variantes e do grupo de controle for menor que o MDE, não será possível detectar essa diferença com um nível de significância alpha e um poder de teste beta especificados.")
 
 st.write("**Equação para cálculo do MDE**")
+st.write("A equação do Mínimo Tamanho Detectável do Efeito (MDE) é:")
 st.latex(r'''
-\text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}
+\text{MDE} = \sqrt{\frac{1}{p-1} \div \frac{n}{16}}
 ''')
+
 
 st.write("**Onde:**")
          
