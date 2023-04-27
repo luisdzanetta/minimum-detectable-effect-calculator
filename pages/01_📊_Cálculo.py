814038fd9f69bb6e749b688e192e7786578b6991
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-27 14:06:34 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -20,7 +20,15 @@ st.write("**Δ:** É a amostra por variante, considerando distribuição proporc
 st.divider()
 
 st.write("**Referência:**")
-st.image("https://github.com/luisdzanetta/repo2/blob/main/kohav.png")
-st.download_button('Baixar o artigo', "https://link.springer.com/content/pdf/10.1007/s10618-008-0114-1.pdf?pdf=button", file_name="Kohav et al. (2009)_Controlled experiments on the web: survey and practical guide")
+def show_pdf("https://github.com/luisdzanetta/repo2/blob/main/Kohav%20et%20al(2009)_Controlled%20experiments%20on%20the%20web.pdf"):
+    with open("https://github.com/luisdzanetta/repo2/blob/main/Kohav%20et%20al(2009)_Controlled%20experiments%20on%20the%20web.pdf","rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+show_pdf('post1-compressed.pdf')
+
+#st.image("https://github.com/luisdzanetta/repo2/blob/main/kohav.png")
+#st.download_button('Baixar o artigo', "https://link.springer.com/content/pdf/10.1007/s10618-008-0114-1.pdf?pdf=button", file_name="Kohav et al. (2009)_Controlled experiments on the web: survey and practical guide")
 
         
